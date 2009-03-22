ebcc383a124a2bf3615e153b9f2e7e89dab53202
Matt Fawcett <mail@matthewfawcett.co.uk>
2009-03-22 13:41:56 +0000

    allow for phpbb tables to have a prefix

--- app/models/phpbb_user.rb
+++ app/models/phpbb_user.rb
@@ -1,6 +1,6 @@
 class PhpbbUser < ActiveRecord::Base
   handles_connection_for "phpbb_database_#{RAILS_ENV}"
+  def self.table_name() "#{PHPBB_AUTH_FORUM_DATABASE_TABLE_PREFIX}users" end
   set_primary_key "user_id"
-
 end
 
